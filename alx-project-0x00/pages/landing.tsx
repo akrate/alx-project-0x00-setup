dc4f53b3f5064775842e3f5cbadbe4041b028ce3
akrate <oussamaakrate590@gmail.com>
2025-09-21 15:26:03 +0100

    test

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -13,3 +13,22 @@ const Pill: React.FC<PillProps> = ({ title }) => {
 export default Pill;
 
 
+import React from "react";
+import Button from "@/components/Button";
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+
+      <div className="flex flex-col gap-4 max-w-xs">
+        {}
+        <Button title="Small Button" styles="text-sm rounded-sm" />
+        <Button title="Medium Button" styles="text-base rounded-md" />
+        <Button title="Large Button" styles="text-lg rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
