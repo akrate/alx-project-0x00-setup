63faada541d1dce53a3987aa813b1f6fc964844c
akrate <oussamaakrate590@gmail.com>
2025-09-21 15:31:35 +0100

    test

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,32 +1,14 @@
-import { PillProps } from "@/interfaces";
-import React from 'react';
-
-
-const Pill: React.FC<PillProps> = ({ title }) => {
-  return (
-    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className="text-sm">{title}</p>
-    </div>
-  );
-};
-
-export default Pill;
-
-
-import React from "react";
+import Card from "@/components/Card";
 import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div>
-      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
-
-      <div className="flex flex-col gap-4 max-w-xs">
-        {}
-        <Button title="Small Button" styles="text-sm rounded-lg" />
-        <Button title="Medium Button" styles="text-base rounded-md" />
-        <Button title="Large Button" styles="text-lg rounded-full" />
-      </div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <Button title="Small Rounded" className="Small" styles="rounded-sm" />
+      <Button title="Small Rounded" className="Medium" styles="rounded-lg" />
+      <Button title="Small Rounded" className="Large" styles="rounded-full" />
     </div>
   );
 };
