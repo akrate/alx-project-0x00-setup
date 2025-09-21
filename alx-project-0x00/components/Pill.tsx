94c6e8bd508dcbb723bec5e9ea21501d9994f28e
akrate <oussamaakrate590@gmail.com>
2025-09-21 14:54:53 +0100

    test

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,13 @@
-const Pill: React.FC<{ title: string }> = ({ title }) => {
+import React from "react"
+
+interface PillProps {
+  title: string;
+}
+
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className="text-sm">{title}</p>
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
     </div>
   )
 }
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,12 +1,19 @@
+import Card from "../components/Card"
 import React from 'react';
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
+
+      {/* Add Card component here */}
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
 
 export default Landing;
+
 
